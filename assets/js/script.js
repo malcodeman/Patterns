4e432f420d1e20d69c660a545c19667f18be6fa8
malcodeman <malcodeman@users.noreply.github.com>
2017-08-03 18:50:09 +0200

    Added few more patterns

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,10 +4,15 @@ function getPatterns() {
         '{"name":"Ahoy","author":"Lorena G", "url":"url(assets/images/backgrounds/ahoy.jpg)" },' +
         '{"name":"Alchemy","author":"Anton Repponen", "url":"url(assets/images/backgrounds/alchemy.gif)" },' +
         '{"name":"Asteroids","author":"Sanja Kusturica", "url":"url(assets/images/backgrounds/asteroids.jpg)" },' +
+        '{"name":"Pink Dust","author":"Park Chu El", "url":"url(assets/images/backgrounds/pink-dust.png)" },' +        
         '{"name":"Brijan","author":"Brijan Powell", "url":"url(assets/images/backgrounds/brijan.gif)" },' +
         '{"name":"Bunting Flag","author":"Raul Varela", "url":"url(assets/images/backgrounds/bunting-flag.png)" },' +
         '{"name":"Memphis Colorful","author":"Raul Gaitan", "url":"url(assets/images/backgrounds/memphis-colorful.png)" },' +
-        '{"name":"Random Grey Variations","author":"Stefan Aleksić", "url":"url(assets/images/backgrounds/random_grey_variations.png)" },' +
+        '{"name":"Random Grey Variations","author":"Stefan Aleksić", "url":"url(assets/images/backgrounds/random-grey-variations.png)" },' +
+        '{"name":"Gaming","author":"Stephen Gheysens", "url":"url(assets/images/backgrounds/gaming-pattern.png)" },' +
+        '{"name":"Sakura","author":"Aslan Zamanloo", "url":"url(assets/images/backgrounds/sakura.png)" },' +
+        '{"name":"Canvas Orange","author":"Raul Varela", "url":"url(assets/images/backgrounds/canvas-orange.jpg)" },' +            
+        '{"name":"Worms","author":"Tomislava Babić", "url":"url(assets/images/backgrounds/wormz.png)" },' +
         '{"name":"Bicycles","author":"Shaun Fox", "url":"url(assets/images/backgrounds/bicycles.png)" }]}';
     return patterns;
 }
